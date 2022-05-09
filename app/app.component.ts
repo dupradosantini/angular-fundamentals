f9986294f49a753c11115300fe06826f9dec5963
Luis Eduardo <dupradosantini@opus-software.com.br>
2022-05-09 14:04:06 -0300

    Property Binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,9 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      {{ title + '!' }}
+      <h1 [innerHTML]="title"></h1>
+      <img [src]="mander">
+      <input type="text" [value]="name">
       <div>
         {{numberOne + numberTwo}} //Angular does the calculation for us, since they are number values in a component.
       </div>
@@ -20,6 +22,9 @@ export class AppComponent {
   numberOne: number = 1;
   numberTwo: number = 2;
   isHappy: boolean = true;
+  mander: string = "img/mander.jpg";
+  name: string = "Luis";
+
   constructor(){
     this.title = "Ultimate Angular"
   }
