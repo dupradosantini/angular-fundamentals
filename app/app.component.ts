9ea824dff0a2fe124cd488a619a978245a01f052
Luis Eduardo <dupradosantini@opus-software.com.br>
2022-05-10 12:02:12 -0300

    using ngClass

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,29 +1,46 @@
 import { Component } from '@angular/core';
 
+
+interface Passenger{
+  id: number,
+  fullname: string,
+  checkedIn: boolean
+}
+
 @Component({
   selector: 'app-root', //basic root component
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      <input type="text" [value]="name" (input)="handleChange($event.target.value)">
-      <div *ngIf="name.length > 2">
-        Searching for ... {{ name }}
-      </div>
+      <h3> Airline Passengers </h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span class="status" [ngClass]="{
+            'checked-in': passenger.checkedIn,
+            'checked-out': !passenger.checkedIn
+            }"></span>
+           {{ i }}: {{passenger.fullname}}
+        </li>
+      </ul>
     </div>
   `
 })
 export class AppComponent {
   title: string;
 
-  name: string = "";
-
-
-  handleChange(value: string){
-    this.name = value;
-  }
+  passengers: Passenger[] = [
+    {
+      id: 1,
+      fullname: "Stephen",
+      checkedIn: true
+    },
+    {
+      id: 2,
+      fullname: "Luis",
+      checkedIn: false
+    }];
 
   constructor(){
     this.title = "Ultimate Angular"
   }
-
 }
