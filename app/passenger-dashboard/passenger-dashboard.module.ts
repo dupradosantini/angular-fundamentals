21c3feb43775a1b7e3016eba053b67c55ce7c4f3
Luis Eduardo <dupradosantini@opus-software.com.br>
2022-05-11 11:59:46 -0300

    Using presentational components

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
+//containers (smart).
 import { PassengerDashboardComponent } from "./containers/passenger-dashboard/passenger-dashboard.component";
+
+//components.
+import { PassengerCountComponent } from "./components/passenger-count/passenger-count.component";
+import { PassengerDetailComponent } from "./components/passenger-detail/passenger-detail.component";
 
 @NgModule({
   declarations: [
-    PassengerDashboardComponent
+    PassengerDashboardComponent,
+    PassengerCountComponent,
+    PassengerDetailComponent
   ],
   imports:[
     CommonModule
