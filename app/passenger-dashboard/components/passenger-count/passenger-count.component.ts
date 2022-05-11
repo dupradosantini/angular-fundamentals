b6508ecb2228eb0728e351bd012d7263528a1227
Luis Eduardo <dupradosantini@opus-software.com.br>
2022-05-11 13:20:56 -0300

    Revert "Passing data with @Input"
    This reverts commit a9bcdfe0f94eede5b273aa1039835a2b069f686b.

--- app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
+++ app/passenger-dashboard/components/passenger-count/passenger-count.component.ts
@@ -1,22 +1,13 @@
-import { Component, Input} from "@angular/core";
-import { Passenger } from "../../models/passenger.interface";
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'passenger-count',
   template:`
   <div>
-    <h3> Airline Passengers! </h3>
-    <div>
-      Total Checked In: {{checkedInCount()}}/{{items.length}}
-    </div>
+    Count component
   </div>
   `
 })
 export class PassengerCountComponent{
-  @Input()
-  items: Passenger[];
-  checkedInCount(): number{
-    if(!this.items) return;
-    return this.items.filter((passenger: Passenger) => passenger.checkedIn).length;
-  }
+  constructor(){}
 }
--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -6,8 +6,7 @@ import { Passenger } from "../../models/passenger.interface";
   styleUrls: ['passenger-dashboard.component.scss'],
   template: `
    <div>
-      <passenger-count [items]="passengers">
-      </passenger-count>
+      <passenger-count></passenger-count>
       <passenger-detail></passenger-detail>
       <h3> Airline Passengers </h3>
       <ul>
