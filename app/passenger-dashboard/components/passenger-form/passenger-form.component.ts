c75750763c587c2b704d30d8e84fca02a5f393f6
Luis Eduardo <dupradosantini@opus-software.com.br>
2022-05-12 19:51:53 -0300

    Creating form with ngModel & inputs

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -5,9 +5,21 @@ import { Passenger } from "../../models/passenger.interface";
   selector: "passenger-form",
   styleUrls: ["passenger-form.component.scss"],
   template:`
-  <form>
-    Form!
+  <form #form="ngForm" novalidate> // novalidate to use angulars built in validation
     {{ detail | json }}
+    <div>
+      Passenger name:
+      <input
+        name="fullname"
+        [ngModel]="detail?.fullname"> //[]="string" sets the initial value when its available
+    </div>
+    <div>
+      Passenger ID:
+      <input
+        name="id"
+        [ngModel]="detail?.id"> //[]="string" sets the initial value when its available
+    </div>
+    {{ form.value | json }}
   </form>`
 })
 export class PassengerFormComponent{
