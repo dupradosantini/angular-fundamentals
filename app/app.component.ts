b246dbf525ff517471f97287c081cb9e6c488419
Luis Eduardo <dupradosantini@opus-software.com.br>
2022-05-13 13:55:13 -0300

    Styling active routes

--- app/app.component.ts
+++ app/app.component.ts
@@ -12,10 +12,19 @@ interface Passenger{
   styleUrls: ['app.component.scss'],
   template: `
   <div class="app">
-    <a routerLink="/">
+    <nav class="nav">
+    <a
+      routerLink="/"
+      routerLinkActive="active"
+      [routerLinkActiveOptions]="{exact: true}">
       Home
     </a>
-    <a routerLink="/oops">404</a>
+    <a
+      routerLink="/oops"
+      routerLinkActive="active">
+      404
+    </a>
+    </nav>
     <router-outlet></router-outlet>
   </div>
   `
