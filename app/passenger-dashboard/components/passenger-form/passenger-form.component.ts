2c4af4a3da8cd0251261cd965e642b7520acf4b4
Luis Eduardo <dupradosantini@opus-software.com.br>
2022-05-12 21:22:29 -0300

    Using checkbox instead of radio button

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -23,21 +23,10 @@ import { Passenger } from "../../models/passenger.interface";
     <div>
       <label>
         <input
-          type="radio"
-          [value]="true"
+          type="checkbox"
           name="checkedIn"
           [ngModel]="detail?.checkedIn"
           (ngModelChange)="toggleCheckIn($event)">
-          Yes
-      </label>
-      <label>
-        <input
-          type="radio"
-          [value]="false"
-          name="checkedIn"
-          [ngModel]="detail?.checkedIn"
-          (ngModelChange)="toggleCheckIn($event)">
-          No
       </label>
     </div>
 
