0c3cd14b430fde1b30073b97c5f93e53e6666401
Luis Eduardo <dupradosantini@opus-software.com.br>
2022-05-13 14:55:41 -0300

    Route params, data-fetching with SwitchMap

--- app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
+++ app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from "@angular/core";
 import { PassengerDashboardService } from "../../passenger-dashboard.service";
 
 import { Passenger } from "../../models/passenger.interface";
+import { ActivatedRoute, Router, Params } from "@angular/router";
+
+import "rxjs/add/operator/switchMap"
 
 @Component({
   selector: "passenger-viewer",
@@ -18,11 +21,13 @@ import { Passenger } from "../../models/passenger.interface";
 
 export class PassengerViewerComponent implements OnInit {
   passenger: Passenger
-  constructor(private passengerService: PassengerDashboardService){
-  }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private passengerService: PassengerDashboardService){}
   ngOnInit(): void {
-    this.passengerService
-      .getPassenger(1)
+    this.route.params
+      .switchMap((data: Params) =>  this.passengerService.getPassenger(data.id))
       .subscribe((data: Passenger) => this.passenger = data);
   }
   onUpdatePassenger(event: Passenger){
